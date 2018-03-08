b12c79b4f4692b1bf19d35f0ccda64e9972aa02a
nisha987 <nshekhawat1000@gmail.com>
2018-03-09 01:54:02 +0530

    first script

---
+++ Assets/scripts/Chase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour {
+
+    public Transform player;
+
+	void Start () {
+	
+	}
+
+
+	void Update () {
+
+        if (Vector3.Distance(player.position, this.transform.position) < 10) 
+        {
+            Vector3 direction = player.position - this.transform.position;
+            
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                            Quaternion.LookRotation(direction), 0.1f); 
+
+            if (direction.magnitude > 2)
+            {
+                this.transform.Translate(0, 0, 0.05f); 
+            }
+
+        }
+
+    }
+}
+
